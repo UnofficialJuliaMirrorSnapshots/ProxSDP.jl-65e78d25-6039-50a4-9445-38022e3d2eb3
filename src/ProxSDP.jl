5af4458a3de5cd4fc49c5b0d65879fbe1a2e98a4
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:35:00 +0000

    Snapshot of branch master taken on 2019-04-07T21:34:56.822-04:00 from "https://github.com/UnofficialJuliaMirror/ProxSDP.jl-65e78d25-6039-50a4-9445-38022e3d2eb3"

---
+++ src/ProxSDP.jl
@@ -0,0 +1,22 @@
+module ProxSDP
+
+    using MathOptInterface, TimerOutputs
+    using Arpack
+    using Compat
+    using Printf
+
+    include("MOI_wrapper.jl")
+    include("eigsolver.jl")
+    include("structs.jl")
+    include("util.jl")
+    include("printing.jl")    
+    include("scaling.jl")
+    include("pdhg.jl")
+    include("residuals.jl")
+    include("prox_operators.jl")
+
+    MOIU.@model _ProxSDPModelData () (MOI.EqualTo, MOI.GreaterThan, MOI.LessThan) (MOI.Zeros, MOI.Nonnegatives, MOI.Nonpositives, MOI.PositiveSemidefiniteConeTriangle) () (MOI.SingleVariable,) (MOI.ScalarAffineFunction,) (MOI.VectorOfVariables,) (MOI.VectorAffineFunction,)
+
+    Solver(;args...) = MOIU.CachingOptimizer(_ProxSDPModelData{Float64}(), ProxSDP.Optimizer(args))
+
+end
